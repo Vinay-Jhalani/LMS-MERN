7346358b17813ade8c7192363a064cd918dc1907
Vinay Jhalani <VinayJhalani.dev@gmail.com>
2024-11-17 18:26:40 +0530

    Auth done, Currently building Instructor dashboard and In server Cloudinary & multer setup is going on

--- server/server.js
+++ server/server.js
@@ -25,7 +25,7 @@ app.use((err, req, res, next) => {
   console.log(err.stack);
   res.status(500).json({
     success: false,
-    message: "Something went wrong",
+    message: "Something went Wrong",
   });
 });
 
